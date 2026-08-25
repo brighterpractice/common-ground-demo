4d4d2bd0233c91214e3d464f3435fa6b41e736ce
brighterpractice <314649874+brighterpractice@users.noreply.github.com>
2026-08-25 17:03:17 -0600

    Create Common Ground Therapy demo foundation

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://align-therapy-demo.example',
+  output: 'static',
+  trailingSlash: 'always',
+});
